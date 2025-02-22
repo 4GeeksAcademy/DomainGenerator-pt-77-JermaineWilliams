eb3f3bfa3cccf7aad330958f747a73aeb2a6d549
Jermaine Williams <wjermaine608@gmail.com>
2025-02-22 00:54:00 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'our', 'them', 'you', 'she'];
+  let adj = ['great', 'big', 'old', 'fluffy','deep'];
+  let noun = ['jogger', 'racoon','Park', 'Chicago', 'banana'];
+
+  for(let i = 0; i < pronoun.length; i++){
+    console.log( "i=", i)
+    for(let a = 0; a < adj.length; a++){
+      console.log( "a=", a)
+      for(let n= 0; n < noun.length; n++){
+        console.log( "n=", n)
+      console.log(pronoun[i] + adj[a] + noun[n]);
+      };}
+    }
+  
+    
+
